b53015cc28134e944e646fe64353375c6e00a9bb
vikas1234-cyber <vikas140206@gmail.com>
2026-04-20 19:22:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -24,4 +24,72 @@ void playSong(int index);
 void pausePlayback();
 void continuePlayback();
 void playNext();
-void playPrevious()
+void playPrevious();
+
+int main() {
+    int choice;
+
+    // Load fixed songs into memory at the start
+    loadSongs();
+
+    while (1) {
+        printf("\n==== SIMPLE PLAYLIST MANAGER ====\n");
+        printf("1. Show Playlist\n");
+        printf("2. Play a Song\n");
+        printf("3. Pause Song\n");
+        printf("4. Continue Song\n");
+        printf("5. Next Song\n");
+        printf("6. Previous Song\n");
+        printf("7. Exit\n");
+        printf("Select an option: ");
+        scanf("%d", &choice);
+        getchar(); // clear input buffer
+
+        switch (choice) {
+            case 1:
+                showPlaylist();
+                break;
+
+            case 2: {
+                showPlaylist();
+                int songNum;
+                printf("Enter the song number to play: ");
+                scanf("%d", &songNum);
+                getchar();
+
+                if (songNum >= 1 && songNum <= totalSongs)
+                    playSong(songNum - 1);
+                else
+                    printf("Invalid song number. Try again!\n");
+                break;
+            }
+
+            case 3:
+                pausePlayback();
+                break;
+
+            case 4:
+                continuePlayback();
+                break;
+
+            case 5:
+                playNext();
+                break;
+
+            case 6:
+                playPrevious();
+                break;
+
+            case 7:
+                printf("Closing player... See you again!\n");
+                PlaySound(NULL, 0, 0);
+                exit(0);
+
+            default:
+                printf("Invalid input! Please enter a number between 1 and 7.\n");
+        }
+    }
+
+    return 0;
+}
+
